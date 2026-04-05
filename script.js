9b873da8b00645fadf75ea18f03e81b293fb1ae3
NorvanMello <110420594+NorvanMello@users.noreply.github.com>
2026-04-05 18:40:52 -0300

    Update script.js

--- script.js
+++ script.js
@@ -2,14 +2,14 @@ const form = document.querySelector("form")
 const formInput = document.querySelector(".form-input")
 const errorInputImg = document.querySelector(".error-input-img")
 const invalidInputMsg = document.querySelector(".invalid-input-msg")
-const regexEmail = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+const regexEmail = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/i;
 
 if(form && formInput && errorInputImg && invalidInputMsg) {
     form.addEventListener("submit", (e) => {
         e.preventDefault();
         
         const raw = formInput.value;
-        const email = raw.trim().toLowerCase();
+        const email = raw.trim();
         const isValid = regexEmail.test(email);
 
         if(!isValid) {
